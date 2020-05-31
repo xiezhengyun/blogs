367cb45a0536d614a0d25556996d6aea122320b6
谢正云 <1549303595@qq.com>
2020-05-31 22:53:51 +0800

    LRU

--- articles/算法/常用算法实现/1-LRU.js
+++ articles/算法/常用算法实现/1-LRU.js
@@ -1,13 +1,13 @@
-class LRU{
+class LRUCache {
 
-    constructor(maxLength){
+    constructor(maxLength) {
         this.cache = Object.create(null);
         this.keys = [];
         this.maxLength = maxLength;
     }
 
-    get(key){
-        if(this.cache[key]){
+    get(key) {
+        if (this.cache[key]) {
             this.removeKey(key)
             this.keys.push(key)
             return this.cache[key]
@@ -15,40 +15,69 @@ class LRU{
         return -1
     }
 
-    put(key,value){
-        if(this.cache[key]){
+    put(key, value) {
+        if (this.cache[key]) {
             this.cache[key] = value;
             this.removeKey(key);
             this.keys.push(key);
-        }else{
+        } else {
             this.cache[key] = value;
             this.keys.push(key);
-            if(this.keys.length > this.maxLength){
+            if (this.keys.length > this.maxLength) {
                 this.removeCache(this.keys[0])
             }
         }
     }
 
 
-    removeKey(key){
+    removeKey(key) {
         var index = this.keys.indexOf(key);
-        this.keys.splice(index,1)
+        this.keys.splice(index, 1)
     }
 
-    removeCache(key){
+    removeCache(key) {
         this.cache[key] = null;
         this.removeKey(key)
     }
 
 }
 
-var lruCache = new LRU(3);
-lruCache.put('a',1)
-lruCache.put('b',2)
-lruCache.put('c',3)
-lruCache.put('d',4)
-console.log(lruCache.get('a')) 
-lruCache.put('b','b')
-console.log(lruCache.get('b')) 
-console.log(lruCache.get('c')) 
-console.log(lruCache.get('d')) 
+var lruCache = new LRUCache(3);
+lruCache.put('a', 1)
+lruCache.put('b', 2)
+lruCache.put('c', 3)
+lruCache.put('d', 4)
+console.log(lruCache.get('a'))
+lruCache.put('b', 'b')
+console.log(lruCache.get('b'))
+console.log(lruCache.get('c'))
+console.log(lruCache.get('d'))
+
+//----------------map 实现 ------------------、
+
+class LRUCache2 {
+
+    constructor(maxLength) {
+        this.cache = new Map();
+        this.maxLength = maxLength;
+    }
+
+    get(key) {
+        if (this.cache.has(key)) {
+            var temp = this.cache.get(key);
+            this.cache.delete(key);
+            this.cache.set(key, temp);
+            return temp
+        }
+        return -1
+    }
+
+    put(key, value) {
+        if (this.cache.has(key)) {
+            this.cache.delete(key);
+        } else if (this.cache.size >= this.maxLength) {
+            this.cache.delete(this.cache.keys().next().value)
+        }
+        this.cache.set(key, value);
+    }
+}
