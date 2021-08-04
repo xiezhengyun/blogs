a957e8f5887038633e4581de4ddd4e07723f4729
xiezy <xiezy@smartebao.com>
2021-08-04 08:51:51 +0800

    add 判断链表是否有环

---
+++ articles/算法/常用算法实现/链表/linked-list-cycle.js
@@ -0,0 +1,28 @@
+/*
+ * https://leetcode-cn.com/problems/linked-list-cycle/submissions/
+ * 141 判断链表是否有环
+ * 快慢指针
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (head == null || head.next == null) return false;
+  var slow = head;
+  var fast = head.next;
+  while (slow != fast) {
+    if (fast == null || fast.next == null) return false;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return true;
+};
