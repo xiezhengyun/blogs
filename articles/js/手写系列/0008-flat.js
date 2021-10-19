71d00ce27f695e7a36367c8830139fa9f9298ada
xiezy <xiezy@smartebao.com>
2021-10-19 15:59:14 +0800

    add 17. 电话号码的字母组合

--- articles/js/手写系列/0008-flat.js
+++ articles/js/手写系列/0008-flat.js
@@ -1,9 +1,10 @@
 function flat(arr, depth = 1) {
   return depth > 0
     ? arr.reduce((pre, cur) => {
+      console.log(pre,cur)
         return pre.concat(Array.isArray(cur) ? flat(cur, depth - 1) : cur);
       }, [])
     : arr.slice();
 }
 
-console.log(flat([[1], [[1], 2]], 3));
+console.log(flat([['first']], 3));
