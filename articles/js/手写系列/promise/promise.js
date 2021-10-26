9347723b2684006058877ebb2dcb37f9d2d89981
xiezy <xiezy@smartebao.com>
2021-10-26 15:21:10 +0800

    fix: promise

--- articles/js/手写系列/promise/promise.js
+++ articles/js/手写系列/promise/promise.js
@@ -33,11 +33,13 @@ class Promise {
         : err => {
             throw err;
           };
+    // 声明返回的promise2
     let promise2 = new Promise((resolve, reject) => {
       if (this.state === 'fulfilled') {
         setTimeout(() => {
           try {
             let x = onFulfilled(this.value);
+            // resolvePromise函数，处理自己return的promise和默认的promise2的关系
             resolvePromise(promise2, x, resolve, reject);
           } catch (e) {
             reject(e);
@@ -83,9 +85,9 @@ class Promise {
     return this.then(null, fn);
   }
 }
-function resolvePromise(promise2, x, resolve, reject){
+function resolvePromise(promise2, x, resolve, reject) {
   // 循环引用报错
-  if(x === promise2){
+  if (x === promise2) {
     // reject报错
     return reject(new TypeError('Chaining cycle detected for promise'));
   }
@@ -97,20 +99,24 @@ function resolvePromise(promise2, x, resolve, reject){
       // A+规定，声明then = x的then方法
       let then = x.then;
       // 如果then是函数，就默认是promise了
-      if (typeof then === 'function') { 
+      if (typeof then === 'function') {
         // 就让then执行 第一个参数是this   后面是成功的回调 和 失败的回调
-        then.call(x, y => {
-          // 成功和失败只能调用一个
-          if (called) return;
-          called = true;
-          // resolve的结果依旧是promise 那就继续解析
-          resolvePromise(promise2, y, resolve, reject);
-        }, err => {
-          // 成功和失败只能调用一个
-          if (called) return;
-          called = true;
-          reject(err);// 失败了就失败了
-        })
+        then.call(
+          x,
+          y => {
+            // 成功和失败只能调用一个
+            if (called) return;
+            called = true;
+            // resolve的结果依旧是promise 那就继续解析
+            resolvePromise(promise2, y, resolve, reject);
+          },
+          err => {
+            // 成功和失败只能调用一个
+            if (called) return;
+            called = true;
+            reject(err); // 失败了就失败了
+          }
+        );
       } else {
         resolve(x); // 直接成功即可
       }
@@ -119,7 +125,7 @@ function resolvePromise(promise2, x, resolve, reject){
       if (called) return;
       called = true;
       // 取then出错了那就不要在继续执行了
-      reject(e); 
+      reject(e);
     }
   } else {
     resolve(x);
