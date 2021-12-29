91c3dd1fb41cca5cfd6e1d7f235c92afb5357ff2
xiezy <xiezy@smartebao.com>
2021-12-29 10:01:17 +0800

    feat Scheduler

--- articles/js/手写系列/0018-Scheduler.js
+++ articles/js/手写系列/0018-Scheduler.js
@@ -1,39 +1,102 @@
 class Scheduler {
-  constructor(limit = 2) {
-    this.limit = limit;
-    this.queue = []; // 模拟队列，先进先出
-    this.processTasks = [];
+  constructor(count) {
+    this.count = count;
+    // 待运行的任务
+    this.queue = [];
+    // 正在运行的任务
+    this.run = [];
   }
-  addTask(...fns) {
-    this.queue.push(...fns);
-    this.run();
+
+  add(task) {
+    return new Promise((resolve) => {
+      task.resolve = resolve;
+      if (this.run.length < this.count) {
+        // 运行任务
+        this.schedule(task);
+      } else {
+        // 放入待运行队列中
+        this.queue.push(task);
+      }
+    });
   }
-  run() {
-    while (this.processTasks.length < this.limit && this.queue.length > 0) {
-      let task = this.queue.shift();
-      let promise = task().then(() => {
-        this.processTasks.splice(this.processTasks.indexOf(promise), 1);
-        this.run();
-      });
-      this.processTasks.push(promise);
-    }
+
+  schedule(task) {
+    this.run.push(task);
+    task().then(() => {
+      task.resolve();
+      // 移除当前任务
+      this.removeRunTask(task);
+
+      // 增加新的任务
+      if (this.queue.length > 0) {
+        const newTask = this.queue.shift();
+        this.schedule(newTask);
+      }
+    });
+  }
+
+  removeRunTask(task) {
+    const index = this.run.indexOf(task);
+    this.run.splice(index, 1);
   }
 }
 
-var task = function (time, order) {
-  var fn = function () {
-    console.log('开始执行', order);
-    return new Promise(resolve => {
-      setTimeout(resolve, time);
-    }).then(() => console.log('执行完成', order));
-  };
-  return fn;
+const timeout = (time) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+
+const scheduler = new Scheduler(2);
+const addTask = (time, order) => {
+  scheduler.add(() => timeout(time)).then(() => console.log('order', order));
 };
 
-var scheduler = new Scheduler(2);
-scheduler.addTask(task(4000, '1'));
-scheduler.addTask(task(500, '2'));
-scheduler.addTask(task(500, '3'));
-scheduler.addTask(task(400, '4'));
-scheduler.addTask(task(300, '5'));
-scheduler.addTask(task(300, '6'));
+addTask(1000, '1');
+addTask(500, '2');
+addTask(300, '3');
+addTask(400, '4');
+
+// fn([['a', 'b'], ['n', 'm'], ['0', '1']]) => ['an0', 'am0', 'an1', 'am1', 'bn0', 'bm0', 'bn1', 'bm0']
+function f(matrix) {
+  const result = [];
+  const len = matrix.length;
+  function dfs(res, curr) {
+    if (res.length === len) {
+      result.push(res.join(''));
+      return;
+    }
+    for (let i = 0; i < matrix[curr].length; i++) {
+      res.push(matrix[curr][i]);
+      dfs(res, curr + 1);
+      res.pop();
+    }
+  }
+  dfs([], 0);
+  return result;
+}
+
+class U {
+  constructor() {
+    this.promise = Promise.resolve();
+  }
+
+  console(val) {
+    this.promise = this.promise.then(() => {
+      console.log(val);
+    });
+    return this;
+  }
+
+  setTimeout(wait) {
+    this.promise = this.promise.then(() => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, wait);
+      });
+    });
+    return this;
+  }
+}
+const u = new U();
+u.console('breakfast').setTimeout(3000).console('lunch').setTimeout(3000).console('dinner');
