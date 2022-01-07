8aa19f246bf4aa348cbdf8a80506e21aacfee6c5
xiezy <xiezy@smartebao.com>
2022-01-07 16:54:15 +0800

    feat finally

--- articles/js/手写系列/promise/promise.finally.js
+++ articles/js/手写系列/promise/promise.finally.js
@@ -15,3 +15,11 @@ function myFinally(promise, onFinally) {
     }
   );
 }
+
+Promise.prototype.finally = function (callback) {
+  let P = this.constructor;
+  return this.then(
+    value  => P.resolve(callback()).then(() => value),
+    reason => P.resolve(callback()).then(() => { throw reason })
+  );
+};
