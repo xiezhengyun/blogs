16bf3492df2179cb960b8a12986782409a70fa9d
xiezhengyun <zhengyun.xie@wowjoy.cn>
2023-11-09 15:28:38 +0800

    flat

--- articles/js/手写系列/0008-flat.js
+++ articles/js/手写系列/0008-flat.js
@@ -7,6 +7,24 @@ function flat(arr, depth = 1) {
     : arr.slice();
 }
 
+function flat(arr, depth = 1) {
+  if (depth === 0) {
+    return arr.slice();
+  }
+  
+  let result = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flat(arr[i], depth - 1));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  
+  return result;
+}
+
 console.log(flat([['first']], 3));
 
 // 栈
