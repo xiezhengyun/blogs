4525b3c1c68872565646f50c94eef07b0144ebd8
xiezhengyun <zhengyun.xie@wowjoy.cn>
2023-11-20 11:34:36 +0800

    fix setImmediate

--- articles/js/手写系列/0025-node-eventloop.js
+++ articles/js/手写系列/0025-node-eventloop.js
@@ -21,6 +21,14 @@ function foo() {
   console.log(2)
   // 1,2 ,nextTick=>nextTick=>nextTick=>nextTick=>timer1=>promise1
 }
+/**
+ *  timers定时器：本阶段执行已经安排的 setTimeout() 和 setInterval() 的回调函数。
+    pending callbacks待定回调：执行延迟到下一个循环迭代的 I/O 回调。
+    idle, prepare：仅系统内部使用。
+    poll 轮询：检索新的 I/O 事件;执行与 I/O 相关的回调（几乎所有情况下，除了关闭的回调函数，它们由计时器和 setImmediate() 排定的之外），其余情况 node 将在此处阻塞。
+    check 检测：setImmediate() 回调函数在这里执行。
+    close callbacks 关闭的回调函数：一些准备关闭的回调函数，如：socket.on('close', ...)。
+ */
 
 function test() {
   console.log('start')
@@ -30,6 +38,9 @@ function test() {
       console.log('children2-1')
     })
   }, 0)
+  setImmediate(()=>{
+    console.log('setImmediate')
+  })
   setTimeout(() => {
     console.log('children3')
     Promise.resolve().then(() => {
@@ -52,6 +63,7 @@ test()
 // children3
 // children2-1
 // children3-1
+// setImmediate
 
 // 以上代码在node11及浏览器的执行结果(顺序执行宏任务和微任务)
 // start
@@ -61,3 +73,4 @@ test()
 // children2-1
 // children3
 // children3-1
+// setImmediate
